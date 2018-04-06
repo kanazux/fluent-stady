faa891306e97b4a609c59e9ebafb5194b7cfced2
kanazux <alvolivre@live.com>
2018-04-05 23:30:42 -0300

    add word occour

--- word_mapping.py
+++ word_mapping.py
@@ -5,7 +5,7 @@
 
 import sys
 import re
-WORD_RE = re.compile('\w')
+WORD_RE = re.compile('\w+')
 index = {}
 with open(sys.argv[1], encoding='utf-8') as fp:
     for line_no, line in enumerate(fp, 1):
