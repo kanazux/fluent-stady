299b437ec9e2aa168fd3acc33b55ee1e5b0a1fea
kanazux <alvolivre@live.com>
2018-04-05 23:12:02 -0300

    add word occour

---
+++ word_mapping.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+# -*- codigng: utf-8 -*-
+"""Word Mapping."""
+
+
+import sys
+import re
+WORD_RE = re.compile('\w')
+index = {}
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start()+1
+            location = (line_no, column_no)
+            ocurrences = index.get(word, [])
+            ocurrences.append(location)
+            index[word] = ocurrences
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
